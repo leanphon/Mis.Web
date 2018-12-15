338a8450ead8af2f4381e6f722e2dc26e9418789
leanphon <leanphon1688@163.com>
2018-12-15 22:25:36 +0800

    1、调岗

---
+++ Net4.5/MIS.Web/Apps.Model/Utility/LogHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.Model.Utility
+{
+    public class LogHelper
+    {
+        public static readonly string filename = "D:\\mis-log.txt";
+        public static void WriteLog(string msg)
+        {
+            try
+            {
+                FileStream fs = File.Open(filename, FileMode.Append | FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite);
+                byte[] data = System.Text.Encoding.Default.GetBytes(msg);
+
+                fs.Write(data, 0, data.Length);
+
+                fs.Flush();
+                fs.Close();
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public static void Error(string msg)
+        {
+            string fmt = string.Format("[{0}][ERROR]{1}",
+                DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), msg);
+           WriteLog(fmt);
+        }
+
+    }
+}
